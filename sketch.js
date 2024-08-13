8d833739a67c9a61fcd4f4edcd479213cf5afb1d
Naliviaoliveir4 <00001127199158sp@al.educacao.sp.gov.br>
2024-08-13 08:23:00 -0300

    projeto

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+    
+    stroke("blue");
+    fill("red");
+    rect(mouseX, mouseY, 250, 20, 35)
+  }
